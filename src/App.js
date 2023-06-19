f8d05ce557cf886e38a2c7a74ea564b3cf16f106
Jyoti Ranjan Giri <jyotiranjangiri.jsr@gmail.com>
2023-06-19 18:28:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MyComponent from "./components/Scroll";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <MyComponent />
+    </div>
+  );
+}
